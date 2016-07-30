1454cb9703ea732973be989056422530f0b6499e
wikibootup <wikibootup@gmail.com>
2016-07-30 15:24:30 +0900

    add actor model

---
+++ movies/models/actor.py
@@ -0,0 +1,9 @@
+from . import db
+
+class Actor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    link = db.Column(db.String(512))
+
+    def __repr__(self):
+        return self.name
