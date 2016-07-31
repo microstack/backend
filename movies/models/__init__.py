4e1c26d9f2176c10b486c5052935bae78da93c02
wikibootup <wikibootup@gmail.com>
2016-07-31 18:33:13 +0900

    seperate app settings from mvc

--- movies/models/__init__.py
+++ movies/models/__init__.py
@@ -1,13 +1,3 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JSON_AS_ASCII'] = False
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
-
+from settings import db, ma
 from . import movie
 from . import actor
