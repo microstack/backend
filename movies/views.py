ea5a974793dc74a44aa679a27d84fcac3ce95096
wikibootup <wikibootup@gmail.com>
2016-07-31 19:00:30 +0900

    flask version rest api

--- movies/views.py
+++ movies/views.py
@@ -2,6 +2,7 @@ import requests
 import os
 
 from models.movie import Movie, movie_list_schema, movies_schema, movie_schema
+from models.actor import Actor, actors_schema, actor_schema
 
 from flask import jsonify, json
 
@@ -12,4 +13,16 @@ from settings import app
 def movie_list():
     data = Movie.query.all()
     result = movie_list_schema.jsonify(data)
+    return result
+
+@app.route('/movies/<id>/', methods=['GET'])
+def movie_detail(id):
+    data = Movie.query.filter_by(id=id).first()
+    result = movie_schema.jsonify(data)
+    return result
+
+@app.route('/movies/<id>/actors', methods=['GET'])
+def movie_actors(id):
+    data = Movie.query.filter_by(id=id).first()
+    result = actors_schema.jsonify(data.actors)
     return result
