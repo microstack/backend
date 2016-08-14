f1663b766a093e5024e53b709abf6e8c289c9566
wikibootup <wikibootup@gmail.com>
2016-08-14 18:53:49 +0900

    add backend port for develop env

--- movies/settings.py
+++ movies/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -9,8 +11,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+BACKEND_MOVIES_PORT = os.environ.get('BACKEND_MOVIES_PORT') or 5000
+
 
 def set_project_paths():
     import os, sys
-#    sys.path.append(os.path.abspath('.'))
     sys.path.append(os.path.abspath('..'))
