e49ac709097de5da014da60b97abc7c349ef80ce
wikibootup <wikibootup@gmail.com>
2016-08-15 21:46:20 +0900

    add port specification in wsgi

--- movies/wsgi.py
+++ movies/wsgi.py
@@ -1,7 +1,9 @@
-from settings import set_project_paths
+from settings import set_project_paths, BACKEND_MOVIES_PORT
 set_project_paths()
 
 
 from api import app
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=BACKEND_MOVIES_PORT)
