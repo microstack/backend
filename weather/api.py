b8b2c6528d6e2303a7acb545f436b25e15a2c557
wikibootup <wikibootup@gmail.com>
2016-08-30 17:20:27 +0900

    add today api

--- weather/api.py
+++ weather/api.py
@@ -1,5 +1,7 @@
 # -*- encoding: utf-8 -*-
 
+from datetime import date
+
 from models import Publish, publish_schema, publish_list_schema
 from models import Weather, weather_list_schema
 
@@ -34,6 +36,23 @@ class PublishWeather(Resource):
         return weather_list_schema.dump(weather_data).data
 
 
+class Today(Resource):
+    def get(self):
+        '''
+        if today query not exist, it means
+        not updated yet(local or rss).
+        '''
+        today = date.today().isoformat()
+        data = Publish.query.filter_by(date=today)
+        weather_data = []
+        if data.count() > 0:
+            data = data[0]
+            weather_data = list(data.weathers)
+            print(weather_data)
+
+        return weather_list_schema.dump(weather_data).data
+ 
+
 from flask_restful import Api
 from settings import app
 
@@ -41,3 +60,4 @@ api = Api(app)
 api.add_resource(PublishList, '/weather/publishes/')
 api.add_resource(PublishDetail, '/weather/publishes/<string:date>/')
 api.add_resource(PublishWeather, '/weather/publishes/<string:date>/weather/')
+api.add_resource(Today, '/weather/today/')
